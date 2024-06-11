587933c2c5aa214b0fd4fba01fb8a8725024980d
Zzc200304111818 <Zzc200304111818@outlook.com>
2024-06-11 15:28:50 +0800

    移动端适配

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
 <script setup></script>
 
-<template></template>
+<template>
+	<div>
+		<van-button type="primary">主要按钮</van-button>
+		<van-button type="success">成功按钮</van-button>
+		<van-button type="default">默认按钮</van-button>
+		<van-button type="danger">危险按钮</van-button>
+		<van-button type="warning">警告按钮</van-button>
+	</div>
+</template>
+<style>
+	div {
+		text-align: center;
+	}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import { Button } from 'vant'
+import 'vant/lib/index.css'
+import "amfe-flexible"
+const app = createApp(App)
+app.use(Button)
+app.mount('#app')
