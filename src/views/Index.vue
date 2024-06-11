c5a92b3dd011fed762ce1642040287a349398e86
Zzc200304111818 <Zzc200304111818@outlook.com>
2024-06-12 01:42:59 +0800

    更改了渐变背景

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,14 +1,15 @@
 <template>
-    <div style="background-color: dodgerblue;" >
+    <div style="background: linear-gradient(to right, #3296fa, #7744ff, #3296fa);">
         <van-space direction="vertical" fill>
             <van-image round width="6rem" height="6rem" style="margin: 2rem;"
                 src="https://www.logosc.cn/uploads/output/2021/06/05/e88d5a43a9ce63ee83344387f1170fa2.jpg?t=1622906000" />
         </van-space>
     </div>
     <div>
-        <van-button type="primary" to="/register" round size="large"
-                color="linear-gradient(to right, #ff6034, #ee0a24)" style="margin-top: 1.2rem;width: 80%;left: 10%;right: 10%;">注册</van-button>
-            <van-button type="primary" to="/login" round size="large" style="margin-top: 1.2rem;width: 80%;left: 10%;right: 10%;">登录</van-button>
+        <van-button type="primary" to="/register" round size="large" color="linear-gradient(to right, #ff6034, #ee0a24)"
+            style="margin-top: 1.2rem;width: 80%;left: 10%;right: 10%;">注册</van-button>
+        <van-button type="primary" to="/login" round size="large"
+            style="margin-top: 1.2rem;width: 80%;left: 10%;right: 10%;">登录</van-button>
     </div>
 
 </template>
