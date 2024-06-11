37c2ae8198e6ba2366e8efc43474ce6208ad8349
loop939 <2181562899@qq.com>
2024-06-12 01:12:14 +0800

    index布局优化

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,13 +1,14 @@
 <template>
-    <div>
+    <div style="background-color: dodgerblue;" >
         <van-space direction="vertical" fill>
-            <van-image round width="10rem" height="10rem"
+            <van-image round width="6rem" height="6rem" style="margin: 2rem;"
                 src="https://www.logosc.cn/uploads/output/2021/06/05/e88d5a43a9ce63ee83344387f1170fa2.jpg?t=1622906000" />
-
-            <van-button type="primary" to="/register" round size="large"
-                color="linear-gradient(to right, #ff6034, #ee0a24)">注册</van-button>
-            <van-button type="primary" to="/login" round size="large">登录</van-button>
         </van-space>
+    </div>
+    <div>
+        <van-button type="primary" to="/register" round size="large"
+                color="linear-gradient(to right, #ff6034, #ee0a24)" style="margin-top: 1.2rem;width: 80%;left: 10%;right: 10%;">注册</van-button>
+            <van-button type="primary" to="/login" round size="large" style="margin-top: 1.2rem;width: 80%;left: 10%;right: 10%;">登录</van-button>
     </div>
 
 </template>
