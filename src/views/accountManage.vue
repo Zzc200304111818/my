c339dd65b6f3c11a05c69799d6c6fb1da93716f6
Zzc200304111818 <Zzc200304111818@outlook.com>
2024-06-12 21:39:05 +0800

    修改vue

--- src/views/accountManage.vue
+++ src/views/accountManage.vue
@@ -2,7 +2,8 @@
     <HeaderButton :title="title"></HeaderButton>
 </template>
 <script setup>
-import HeaderButton from '@/components/HeaderButton.vue';
+import HeaderButton from '@/components/HeaderButton.vue'
 import { ref } from 'vue'
 const title = ref('账号管理')
+const hello = null
 </script>
--- src/views/setting.vue
+++ src/views/setting.vue
@@ -1,9 +1,10 @@
 <template>
     <HeaderButton :title="title"></HeaderButton>
-    <van-cell title="账号管理"  is-link to="/mine/setting/accountManage" size="large" />
+    <van-cell title="账号管理" is-link to="/mine/setting/accountManage" size="large" />
 </template>
 <script setup>
-import HeaderButton from '@/components/HeaderButton.vue';
+import HeaderButton from '@/components/HeaderButton.vue'
 import { ref } from 'vue'
 const title = ref('设置')
+const hello = null
 </script>
