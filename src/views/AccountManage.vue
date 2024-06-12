2069216211baeba5d43d45248da47a58d26356c2
Zzc200304111818 <Zzc200304111818@outlook.com>
2024-06-12 22:03:29 +0800

    重建

---
+++ src/views/AccountManage.vue
@@ -0,0 +1,8 @@
+<template>
+    <HeaderButton :title="title"></HeaderButton>
+</template>
+<script setup>
+import HeaderButton from '@/components/HeaderButton.vue'
+import { ref } from 'vue'
+const title = ref('账号管理')
+</script>
