7bb2d60c578eb69357d65a57bd7a029975dce58d
Zzc200304111818 <Zzc200304111818@outlook.com>
2024-06-12 22:01:20 +0800

    删除

--- src/views/accountManage.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <HeaderButton :title="title"></HeaderButton>
-</template>
-<script setup>
-import HeaderButton from '@/components/HeaderButton.vue'
-import { ref } from 'vue'
-const title = ref('账号管理')
-</script>
--- src/views/setting.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <HeaderButton :title="title"></HeaderButton>
-    <van-cell title="账号管理" is-link to="/mine/setting/accountManage" size="large" />
-</template>
-<script setup>
-import HeaderButton from '@/components/HeaderButton.vue'
-import { ref } from 'vue'
-const title = ref('设置')
-</script>
