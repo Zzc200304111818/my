edcce25aa007ca1afb72a7e84c4e1031ed2ff1a4
X1el <xl3542766237@163.com>
2024-06-13 17:11:45 +0800

    楼层管理和任务

--- src/views/Floor.vue
+++ src/views/Floor.vue
@@ -13,95 +13,48 @@
       <van-swipe-item>12点后楼层断电。</van-swipe-item>
     </van-swipe>
   </van-notice-bar>
-
-  <van-cell-group inset>
-    <van-cell title="入住校区楼栋" value="" />
-    <van-cell title="下列更改校区楼号信息："  label="修改后点击按钮提交" />
-  </van-cell-group>
-
-  <van-field
-    v-model="fieldValue"
+  <form @submit.prevent="">
+    <van-field
+    v-model="result"
     is-link
     readonly
-    label="校区楼号选择："
-    placeholder="请选择所在校区和楼号"
-    @click="show = true"
+    name="picker"
+    label="选择器"
+    placeholder="点击选择城市"
+    @click="showPicker = true"
   />
-  <van-popup v-model:show="show" round position="bottom">
-    <van-cascader
-      v-model="cascaderValue"
-      title="请选择所在校区和楼号"
-      :options="options"
-      @close="show = false"
-      @finish="onFinish"
+  <van-popup v-model:show="showPicker" position="bottom">
+    <van-picker
+      :columns="columns"
+      @confirm="onConfirm"
+      @cancel="showPicker = false"
     />
   </van-popup>
 
-  <van-cell-group inset>
-    <van-cell title="下列选择楼层：" value="" label="修改后点击按钮提交"/>
-  </van-cell-group>
-
-  <van-dropdown-menu>
-    <van-dropdown-item v-model="value1" :options="option1" />
-  </van-dropdown-menu>
-
-  <van-button type="primary" block>提交</van-button>
+  <van-button type="primary" block native-type="submit">提交</van-button>
+  </form>
 </template>
 <script setup>
-import { ref } from "vue";
-import { showToast } from "vant";
+import { ref } from "vue"
+
 import HeaderButton from "@/components/HeaderButton.vue";
-const title = ref("楼层管理");
+const title = ref("楼层管理")
+const result = ref("");
+const showPicker = ref(false)
+console.log(result.value)
+const columns = [
+  { text: "杭州", value: "Hangzhou" },
+  { text: "宁波", value: "Ningbo" },
+  { text: "温州", value: "Wenzhou" },
+  { text: "绍兴", value: "Shaoxing" },
+  { text: "湖州", value: "Huzhou" },
+]
 
-const show = ref(false);
-const fieldValue = ref("");
-const cascaderValue = ref("");
-// 选项列表，children 代表子选项，支持多级嵌套
-const options = [
-  {
-    text: "蛟桥园",
-    value: "330000",
-    children: [{ text: "1楼", value: "330100" }],
-  },
-  {
-    text: "枫林园",
-    value: "330000",
-    children: [{ text: "1楼", value: "330100" }],
-  },
-  {
-    text: "麦庐园",
-    value: "320000",
-    children: [
-      { text: "宁庐", value: "a" },
-      {
-        text: "静庐",
-        value: "b",
-        children: [
-          { text: "A栋", value: "1" },
-          { text: "B栋", value: "2" },
-          { text: "C栋", value: "3" },
-          { text: "D栋", value: "4" },
-          { text: "E栋", value: "5" },
-        ],
-      },
-      { text: "祥庐", value: "c" },
-      { text: "和庐", value: "d" },
-    ],
-  },
-];
-// 全部选项选择完毕后，会触发 finish 事件
-const onFinish = ({ selectedOptions }) => {
-  show.value = false;
-  fieldValue.value = selectedOptions.map((option) => option.text).join("/");
-};
-
-const value1 = ref(0);
-const option1 = [
-  { text: "1楼", value: 0 },
-  { text: "2楼", value: 1 },
-  { text: "3楼", value: 2 },
-  { text: "4楼", value: 3 },
-  { text: "5楼", value: 4 },
-  { text: "6楼", value: 5 },
-];
+const onConfirm = ({ selectedOptions }) => {
+  result.value = selectedOptions[0]?.text;
+  showPicker.value = false;
+}
+const handleSubmit = () => {
+  conoel.log(reult.value)
+}  
 </script>
